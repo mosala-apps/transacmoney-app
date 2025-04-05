31b3a76d9a1f485559e6975dc2c54ca55770ff05
rkota( Rosaire Kota) <rosairekota@anicns.gouv.cd>
2025-04-05 18:55:58 +0100

    refactor:update config

--- config/ApiURL.ts
+++ config/ApiURL.ts
@@ -1,1 +1,2 @@
-export const API_URL: string ='http://localhost:4500/api'
+export const API_URL_LOCAL: string = "http://localhost:4500/api";
+export const API_URL:string=  "http://87.98.140.161:4500/api";
